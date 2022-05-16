d50c6e9fb885e6dbc42cf60749d75a02c77d8ee9
cheese.lee <cheese.lee@kakaoenterprise.com>
2022-05-16 23:15:42 +0900

    implementing async endpoint object routing

--- main.py
+++ main.py
@@ -1,15 +1,95 @@
+from gc import callbacks
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from fastapi.concurrency import run_in_threadpool
+from pydantic import BaseModel, AnyUrl
+from enum import Enum
+from typing import List, Dict
+from starlette.responses import JSONResponse
 
+import uvicorn
 import json
-import httpx
+import aiohttp
 import asyncio
+import jsonpatch
+import requests
 
-app = FastAPI()
+
+class MethodEnum(str, Enum):
+    GET = "GET"
+    POST = "POST"
+    DELETE = "DELETE"
+    UPDATE = "UPDATE"
+    PATCH = "PATCH"
+
+
+class BackendInfo(BaseModel):
+    url: AnyUrl
+    method: MethodEnum
+    header_json_patch: Dict = None
+    body_json_patch: Dict = None
+
+
+class RouteInfo(BaseModel):
+    route: str
+    method: MethodEnum
+    backends: List[BackendInfo]
+
+
+class GlobalConfig(BaseModel):
+    port: int
+    routes: List[RouteInfo]
+
+
+routes = []
+
+
+
+class BackendAggregator:
+    def __init__(self, routeInfo: RouteInfo):
+        self.backends = routeInfo.backends
+
+    async def __call__(self):
+        return await http_client.get(self.backends[0].url).json()
 
 with open("config.json", "r") as configFile:
-    config = json.load(configFile)
+    config = GlobalConfig(**json.load(configFile))
+    # print(config)
 
-for route_info in config["routes"]:
-    app.add_api_route(
-        path=route_info["route"], endpoint=None, methods=route_info["method"]
-    )
+    for route_info in config.routes:
+        backend_aggregator = BackendAggregator(route_info)
+        routes.append(
+            APIRoute(
+                route_info.route,
+                backend_aggregator,
+                methods=[route_info.method],
+                response_class=JSONResponse,
+            )
+        )
+
+app = FastAPI(routes=routes)
+
+class HttpClient:
+    session: aiohttp.ClientSession = None
+
+    def start(self):
+        self.session = aiohttp.ClientSession()
+
+    async def stop(self):
+        await self.session.close()
+        self.session = None
+
+    def __call__(self) -> aiohttp.ClientSession:
+        assert self.session is not None
+        return self.session
+
+http_client = HttpClient()
+
+@app.on_event("startup")
+async def startup():
+    http_client.start()
+
+
+
+if __name__ == "__main__":  # local dev
+    uvicorn.run(app, host="0.0.0.0", port=8000)
