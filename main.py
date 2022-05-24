8d7d43a653990f93f5efb0675ba3d96dffa7f069
cheese.lee <cheese.lee@kakaoenterprise.com>
2022-05-24 21:08:47 +0900

    testing multi backends case

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
-from fastapi import FastAPI, Depends, Header, WebSocket
+from fastapi import FastAPI, Request
 from fastapi.routing import APIRoute
 from fastapi.responses import HTMLResponse
 from fastapi.concurrency import run_in_threadpool
 from httpx import AsyncClient
 
+import jsonpatch
+
 app = FastAPI()
+
+#httpx not follow redirects by default.
+
  
 @app.get('/test1/{param_1}/{param_2}')
-async def call__test1__param_1___param_2_(param_1,param_2,):
+async def call__test1__param_1___param_2_(param_1,param_2,request: Request):
+    headers = request.headers
+    body = request.body
     async with AsyncClient() as client: 
-        res = await client.get('http://localhost:1337/api/services')
-        return res.json()
+        response = await client.get(f'http://localhost:1337/api/services')
+        if  not response.is_success:
+            print ('not success!')
+            return response.json()
+
+        headers = response.headers
+        body = response.json()
+
+ 
+        response = await client.get(f'http://localhost:1337/api/products')
+        if  not response.is_success:
+            print ('not success!')
+            return response.json()
+
+        headers = response.headers
+        body = response.json()
+
+
+        return body
+        
+
  
 @app.get('/test2')
-async def call__test2():
+async def call__test2(request: Request):
+    headers = request.headers
+    body = request.body
     async with AsyncClient() as client: 
-        res = await client.get('http://localhost:1337/api/products')
-        return res.json()
+        response = await client.get(f'http://localhost:1337/api/products')
+        if  not response.is_success:
+            print ('not success!')
+            return response.json()
+
+        headers = response.headers
+        body = response.json()
+
+
+        return body
+        
+
--- templates/main_template.py
+++ templates/main_template.py
@@ -1,20 +1,45 @@
-from fastapi import FastAPI, Depends, Header, WebSocket
+from fastapi import FastAPI, Request
 from fastapi.routing import APIRoute
 from fastapi.responses import HTMLResponse
 from fastapi.concurrency import run_in_threadpool
 from httpx import AsyncClient
 
+import jsonpatch
+
 app = FastAPI()
+
+#httpx not follow redirects by default.
 
 {% for route in routes %} 
 @app.{{route['method'].lower()}}('{{route["route"]}}')
 async def call_{{route["route"].replace('/','_').replace('{','_').replace('}','_')}}(
     {%- for param in route["route"].split('{') if '}' in param -%}
     {{param.replace('}','').replace('/','')}},
-    {%- endfor -%}):
+    {%- endfor -%}request: Request):
+    headers = request.headers
+    body = request.body
     async with AsyncClient() as client:
     {%- for backend in route['backends'] %} 
-        res = await client.{{backend['method'].lower()}}('{{backend["host"]}}{{backend["url"]}}')
-        return res.json()
-    {% endfor -%}
+        response = await client.{{backend['method'].lower()}}(f'{{backend["host"]}}{{backend["url"]}}')
+        if  not response.is_success:
+            print ('not success!')
+            return response.json()
+
+        headers = response.headers
+        body = response.json()
+
+        {%- if 'header_json_patch' in backend %}
+        header_patch = jsonpatch.JsonPatch(backend['header_json_patch'])
+        headers = header_patch.apply(headers)
+        {% endif %}
+
+        {%- if 'body_json_patch' in backend %}
+        body_patch = jsonpatch.JsonPatch(backend['body_json_patch'])
+        body = body_patch.apply(body)
+        {% endif %}
+
+    {% endfor %}
+        return body
+        
+
 {% endfor %}
